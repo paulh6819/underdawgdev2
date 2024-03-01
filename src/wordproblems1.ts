64f7d938172eb505159ddef5a0da948c3f7dc956
Paul Henderson <paulh6819@gmail.com>
2024-03-01 17:34:04 -0500

    working on config issues and starting problems

---
+++ src/wordproblems1.ts
@@ -0,0 +1,11 @@
+//word problem one
+//What are all of the words containing UU?
+
+import { tempWords } from "../scripts/tempWordList";
+// console.log(tempWords);
+
+function returnsWordsWithUUIncluded(arr: string[]): string[] {
+  return arr.filter((word) => word.includes("UU"));
+}
+
+console.log(returnsWordsWithUUIncluded(tempWords));
---
+++ src/wordproblems2.ts
@@ -0,0 +1,10 @@
+import { tempWords } from "../scripts/tempWordList";
+
+// Words with X, Y, and Z: []
+function returnsWordsWithXYZIncluded(arr: string[]): string[] {
+  return arr.filter(
+    (word) => word.includes("x") && word.includes("y") && word.includes("z")
+  );
+}
+console.log(tempWords);
+console.log(returnsWordsWithXYZIncluded(tempWords));
