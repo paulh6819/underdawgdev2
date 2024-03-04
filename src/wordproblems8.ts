9b85719505b7fd9f32cc19d0e859740faf81404d
Paul Henderson <paulh6819@gmail.com>
2024-03-03 21:33:54 -0500

    finished word problem 8

---
+++ src/wordproblems8.ts
@@ -0,0 +1,21 @@
+import { tempWords } from "../scripts/tempWordList";
+
+// What are all of the words with no vowel and not even a Y?
+
+function wordsWithNoVowelsOrY(arr: string[]): string[] {
+  return arr.filter((word) => {
+    let vowels: string = "aeuioy";
+    let toUpperCaseVowels = vowels.toUpperCase();
+    let splitWord: string[] = word.toUpperCase().split("");
+    for (let char of splitWord) {
+      if (toUpperCaseVowels.includes(char)) {
+        console.log("this should be a vowel:", char);
+        splitWord.pop();
+        break;
+      }
+    }
+    return splitWord.length === word.length;
+  });
+}
+
+console.log(wordsWithNoVowelsOrY(tempWords));
